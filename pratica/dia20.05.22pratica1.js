3a8964a2030dcaef50c04f9f914235da14015094
JonathasGit <jonathas-25@hotmail.com>
2022-05-26 23:38:08 -0300

    pratuca

---
+++ pratica/dia20.05.22pratica1.js
@@ -0,0 +1,16 @@
+var carro = celta;
+var categoria = 1;
+
+switch (categoria) {
+
+    case 0:
+        console.log('Grupo A')
+        break
+    case 1:
+        console.log('Grupo B')
+        break
+
+    default:
+        console.log('Grupo não selecionado')
+        break
+}
